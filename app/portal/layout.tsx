510eff270144868757773be5cd22eb4561b42507
Adrrien04 <adrrien.chandrakumar@gmail.com>
2025-02-19 12:02:36 +0100

    subnavbar changing with status

--- app/portal/layout.tsx
+++ app/portal/layout.tsx
@@ -1,9 +1,11 @@
 import Navbar from '@/app/ui/Components/Navbar';
+import SubNavbar from '@/app/ui/Components/SubNavbar';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children, status }: { children: React.ReactNode, status: 'admin' | 'teacher' | 'student' }) {
     return (
         <div className="flex flex-col h-screen">
             <Navbar />
+            <SubNavbar status={status} />
             <div className="flex flex-grow md:flex-row md:overflow-hidden">
                 <div className="flex-grow p-6 md:overflow-y-auto md:p-12">
                     {children}
