1cbf5af42cc14f4d96cca92a3f6ad4d35c3ae8ef
fluffykoo <oumou.camara@efrei.net>
2025-02-19 11:27:02 +0100

    Chapter4bis

---
+++ app/portal/admin/page.tsx
@@ -0,0 +1,12 @@
+import RootLayout from 'app/portal/layout';
+
+export default function AdminPage() {
+  return (
+    <RootLayout>
+      <div>
+        <h1>Admin Page</h1>
+        {/* Contenu de la page admin */}
+      </div>
+    </RootLayout>
+  );
+}
